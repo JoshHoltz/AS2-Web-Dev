382fb6efabd8c056dd1787090f3f55f0e1493e11
Josh <joshua.holtz@outlook.com>
2024-03-20 18:55:29 +0000

    Init - Template & Start Button

---
+++ as2/script.js
@@ -0,0 +1,119 @@
+let upPressed = false;
+let downPressed = false;
+let leftPressed = false;
+let rightPressed = false;
+
+const main = document.querySelector('main');
+
+//Player = 2, Wall = 1, Enemy = 3, Point = 0
+let maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 0, 1, 0, 0, 0, 0, 3, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 3, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 3, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+//Populates the maze in the HTML
+for (let y of maze) {
+    for (let x of y) {
+        let block = document.createElement('div');
+        block.classList.add('block');
+
+        switch (x) {
+            case 1:
+                block.classList.add('wall');
+                break;
+            case 2:
+                block.id = 'player';
+                let mouth = document.createElement('div');
+                mouth.classList.add('mouth');
+                block.appendChild(mouth);
+                break;
+            case 3:
+                block.classList.add('enemy');
+                break;
+            default:
+                block.classList.add('point');
+                block.style.height = '1vh';
+                block.style.width = '1vh';
+        }
+
+        main.appendChild(block);
+    }
+}
+
+//Player movement
+function keyUp(event) {
+    if (event.key === 'ArrowUp') {
+        upPressed = false;
+    } else if (event.key === 'ArrowDown') {
+        downPressed = false;
+    } else if (event.key === 'ArrowLeft') {
+        leftPressed = false;
+    } else if (event.key === 'ArrowRight') {
+        rightPressed = false;
+    }
+}
+
+function keyDown(event) {
+    if (event.key === 'ArrowUp') {
+        upPressed = true;
+    } else if (event.key === 'ArrowDown') {
+        downPressed = true;
+    } else if (event.key === 'ArrowLeft') {
+        leftPressed = true;
+    } else if (event.key === 'ArrowRight') {
+        rightPressed = true;
+    }
+}
+
+const player = document.querySelector('#player');
+const playerMouth = player.querySelector('.mouth');
+let playerTop = 0;
+let playerLeft = 0;
+
+setInterval(function() {
+    if(downPressed) {
+        playerTop++;
+        player.style.top = playerTop + 'px';
+        playerMouth.classList = 'down';
+    }
+    else if(upPressed) {
+        playerTop--;
+        player.style.top = playerTop + 'px';
+        playerMouth.classList = 'up';
+    }
+    else if(leftPressed) {
+        playerLeft--;
+        player.style.left = playerLeft + 'px';
+        playerMouth.classList = 'left';
+    }
+    else if(rightPressed) {
+        playerLeft++;
+        player.style.left = playerLeft + 'px';
+        playerMouth.classList = 'right';
+    }
+}, 10);
+
+// document.addEventListener('keydown', keyDown);
+// document.addEventListener('keyup', keyUp);
+
+// START BUTTON 
+const startButton = document.querySelector('.start');
+
+function startGame() {
+    // startButton.firstChild.nodeValue = 'changed';
+    startButton.style.display = 'none';
+    document.addEventListener('keydown', keyDown);
+    document.addEventListener('keyup', keyUp);
+
+    console.log('Game Started')
+}
+
+startButton.addEventListener('click', startGame)
